f960b273740f43e18fe255c2c4dc31eb32ce49f7
Fernando Vallejo <fervalle200190@gmail.com>
2022-11-03 11:35:30 -0400

    second-import-fixed

--- src/ispeak/components/AddModuleModal.jsx
+++ src/ispeak/components/AddModuleModal.jsx
@@ -3,7 +3,7 @@ import { useContext, useState } from "react";
 import { useForm } from "../../hooks";
 import { postModules } from "../../utils";
 import { CourseContext } from "../context";
-import { ModalLayout } from "../layout/modalLayout";
+import { ModalLayout } from "../layout/ModalLayout";
 import { SelectOptions } from "./SelectOptions";
 import { SnackBarComponent } from "./SnackBarComponent";
 
